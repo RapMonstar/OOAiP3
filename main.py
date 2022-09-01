6bee23c654ebfd380232c09c83051df7b5550488
RepMonstar <azaza.lekha@mail.ru>
2022-09-01 13:14:46 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,98 @@
+# команда
+# Переключатель настольной лампы и музыкальной станции
+from typing import List
+from abc import ABC, abstractmethod
+
+class ICommand:
+    @abstractmethod
+    def execute(self) -> None:
+        ...
+
+
+class Light:
+    def onLight(self):
+        print('Лампа включается')
+
+    def offLight(self):
+        print('Лампа выключается')
+
+
+class MusicCenter:
+    def onMusicCenter(self):
+        print('Музыкальный центр включается')
+
+    def offMusicCenter(self):
+        print('Музыкальный центр выключается')
+
+    def changeSong(self):
+        print('Изменение песни')
+
+
+class PrepareOnLight(ICommand):
+    def __init__(self, executor: Light):
+        self.__executor = executor
+
+    def execute(self) -> None:
+        self.__executor.onLight()
+
+
+class PrepareOffLight(ICommand):
+    def __init__(self, executor: Light):
+        self.__executor = executor
+
+    def execute(self) -> None:
+        self.__executor.offLight()
+
+
+class PrepareOnMusicCenter(ICommand):
+    def __init__(self, executor: MusicCenter):
+        self.__executor = executor
+
+    def execute(self) -> None:
+        self.__executor.onMusicCenter()
+
+
+class PrepareOffMusicCenter(ICommand):
+    def __init__(self, executor: MusicCenter):
+        self.__executor = executor
+
+    def execute(self) -> None:
+        self.__executor.offMusicCenter()
+
+
+class PrepareChangeSong(ICommand):
+    def __init__(self, executor: MusicCenter):
+        self.__executor = executor
+
+    def execute(self) -> None:
+        self.__executor.changeSong()
+
+
+class Gadgets:
+    def __init__(self):
+        self.history: List[ICommand] = []
+
+    def addCommand(self, command: ICommand) -> None:
+        self.history.append(command)
+
+    def work(self) -> None:
+        if not self.history:
+            print('Не задана очерёдность работы предметов')
+        else:
+            for executor in self.history:
+                executor.execute()
+        self.history.clear()
+
+
+if __name__ == '__main__':
+    light = Light()
+    musicCenter = MusicCenter()
+    gadget = Gadgets()
+
+    gadget.addCommand(PrepareOnLight(light))
+    gadget.addCommand(PrepareOnMusicCenter(musicCenter))
+    gadget.addCommand(PrepareChangeSong(musicCenter))
+    gadget.addCommand(PrepareOffMusicCenter(musicCenter))
+    gadget.addCommand(PrepareOffLight(light))
+
+    gadget.work()
